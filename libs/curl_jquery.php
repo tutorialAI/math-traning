b58cf994c2da68dca98c33c91f220916946abc7c
tutorialAI <34983545+tutorialAI@users.noreply.github.com>
2018-10-10 16:11:06 +0300

    don't work prototype curl file

--- libs/curl_jquery.php
+++ libs/curl_jquery.php
@@ -1,23 +1,33 @@
 <?php
-	function curl_get($url, $referer = "http://www.google.com/"){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($ch, CURLOPT_POST, TRUE);
-		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Geco/20100101 Firefox/58.0.2");
-		// curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($personalData));
-		// curl_setopt($ch, CURLOPT_PROXY, "195.112.122.197");
-		// curl_setopt($ch, CURLOPT_PROXYPORT, '45050');
-		// curl_setopt($ch, CURLOPT_COOKIEJAR, "d://cooki.txt");
-		// curl_setopt($ch, CURLOPT_COOKIEFILE,"d://cooki.txt");
-		curl_setopt($ch, CURLOPT_TIMEOUT, 9);
-		// curl_setopt($ch, CURLOPT_CONNTIMEOUT, 6);
-		curl_setopt($ch,CURLOPT_REFERER, $referer);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, flase);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$data = curl_exec($ch);
-		curl_close($ch);
-		return $data;
-	}
+function curl_get($url, $referer = "www.google.com/")
+{
+		$proxies = ["89.250.10.14:56422","191.36.172.66:55952"];
+		$steps = count($proxies);
+		$step = 0;
+		$try = true;
+		while($try){
+				// create curl resource
+				$ch = curl_init();
+				$proxy = isset($proxies[$step]) ? $proxies[$step] : null;
+
+				curl_setopt($ch, CURLOPT_HEADER, 0);
+				curl_setopt($ch, CURLOPT_REFERER, $referer);
+				curl_setopt($ch, CURLOPT_USERAGENT, "Opera/9.80 (Windows NT 5.1; U; ru) Presto/2.9.168 Version/11.51");
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_PROXY, $proxy);
+				curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //return the transfer as a string
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+				$output = curl_exec($ch); // get content
+				$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE); // Получаем HTTP-код
+
+				// close curl resource to free up system resources
+				curl_close($ch);
+
+				$step++;
+				$try = (($step < $steps) && ($http_code != 200));
+		}
+		return $output;
+}
 ?>
